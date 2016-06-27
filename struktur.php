4f266eef2d61d22dc52f974adfca5173bd3f775b
Jorge Fernandez <jorge.konstanz@gmail.com>
2016-06-27 10:44:06 +0200

    Version 0.85.2

--- struktur.php
+++ struktur.php
@@ -19,17 +19,49 @@
                 <ul>
                     <li><i class="fa fa-folder-o" aria-hidden="true"></i> Bower_Components</li>
                     <li><i class="fa fa-folder-o" aria-hidden="true"></i> images</li>
-                    <li><i class="fa fa-folder-o" aria-hidden="true"></i> js</li>
+                    <li><i class="fa fa-folder-o" aria-hidden="true"></i> js
+                    <ul>
+                        <li>application.js</li>
+                        <li>effect.js</li>
+                        <li>application.js</li>
+                        <li>script.js</li>
+                        <li></li>
+                    </ul>
+                    </li>
                     <li><i class="fa fa-folder-o" aria-hidden="true"></i> scss
                         <ul>
 
-                            <li></li>
-                            <li></li>
+                            <li>bg.scss</li>
+                            <li>bg-images.scss</li>
+                            <li>bootstrap-cambios.scss</li>
+                            <li>code.scss</li>
+                            <li>compatible.scss</li>
+                            <li>content.scss</li>
+                            <li>factory.scss</li>
+                            <li>font-colors.scss</li>
+                            <li>fonts.scss</li>
+                            <li>footer.scss</li>
+                            <li>franse.scss</li>
+                            <li><stronger>globalbit.scss</stronger></li>
+                            <li>grid-gallery.scss</li>
+                            <li>img.scss</li>
+                            <li>img-effects.scss</li>
+                            <li>main.scss</li>
+                            <li>maps.scss</li>
+                            <li>mediumscreen.scss</li>
+                            <li>navbar.scss</li>
+                            <li>smallscreen.scss</li>
+                            <li>strukture.scss</li>
+                            <li>variables.scss</li>
                             <li></li>
                         </ul>
                     </li>
                     <li><i class="fa fa-file-o" aria-hidden="true"></i> bower.json</li>
-                    <li><i class="fa fa-file-o" aria-hidden="true"></i> index.html</li>
+                    <li><i class="fa fa-file-o" aria-hidden="true"></i> index.php</li>
+                    <li><i class="fa fa-file-o" aria-hidden="true"></i> headcode.php</li>
+                    <li><i class="fa fa-file-o" aria-hidden="true"></i> sup.php</li>
+                    <li><i class="fa fa-file-o" aria-hidden="true"></i> footercode.php</li>
+                    <li><i class="fa fa-file-o" aria-hidden="true"></i> footer.php</li>
                     <li></li>
                     <li></li>
                 </ul>
